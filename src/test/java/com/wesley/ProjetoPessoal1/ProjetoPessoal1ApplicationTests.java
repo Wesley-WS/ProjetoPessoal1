aae5f47f94e708401167511463229bf848741c40
Wesley-WS <whesleycosta13@gmail.com>
2021-11-18 16:35:44 -0300

    Projeto Criado

---
+++ src/test/java/com/wesley/ProjetoPessoal1/ProjetoPessoal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.ProjetoPessoal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPessoal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
