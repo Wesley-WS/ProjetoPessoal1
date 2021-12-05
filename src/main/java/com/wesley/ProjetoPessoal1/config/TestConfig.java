56f5964ad9b51d4959ab71031667b982dedde96b
Wesley-WS <whesleycosta13@gmail.com>
2021-12-05 12:37:23 -0300

    Payment, one-to-one association

--- src/main/java/com/wesley/ProjetoPessoal1/config/TestConfig.java
+++ src/main/java/com/wesley/ProjetoPessoal1/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wesley.ProjetoPessoal1.entities.Category;
 import com.wesley.ProjetoPessoal1.entities.Order;
 import com.wesley.ProjetoPessoal1.entities.OrderItem;
+import com.wesley.ProjetoPessoal1.entities.Payment;
 import com.wesley.ProjetoPessoal1.entities.Product;
 import com.wesley.ProjetoPessoal1.entities.User;
 import com.wesley.ProjetoPessoal1.entities.enums.OrderStatus;
@@ -85,6 +86,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z") , o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
